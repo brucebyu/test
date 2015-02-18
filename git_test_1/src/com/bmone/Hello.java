80a5f2e8fb7e3439a529feb5e83d774efcdff8aa
bruce <brucebyu@gmail.com>
2015-02-17 19:56:12 -0800

    2nd commit

--- git_test_1/src/com/bmone/Hello.java
+++ git_test_1/src/com/bmone/Hello.java
@@ -1,7 +1,20 @@
 package com.bmone;
 
 public class Hello {
+	private String name;
+	
+	public Hello(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
 	public static void main(String[] args){
 		System.out.println("HI ....");
+		
+		Hello hello = new Hello("Test");
+		
+		System.out.println("hello name is: "+ hello.getName());
 	}
 }
