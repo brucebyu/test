645f6d9b6812a39c9fc300961fe2e5aa0ec4df43
bruce <brucebyu@gmail.com>
2015-02-19 12:17:14 -0800

    local commit to hello 3

--- git_test_2/src/com/bmone/World.java
+++ git_test_2/src/com/bmone/World.java
@@ -5,6 +5,10 @@ public class World {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi World!!!");
+		System.out.println("Hi World 2!!!");
+		System.out.println("Hi World 3!!!");
+		
+		
 	}
 
 }
