f1bd6307cacd1bc63faee93862a532eb26344398
Bruce Yu <brucebyu@gmail.com>
2015-02-19 12:54:46 -0800

    Update World.java
    remove comments

--- git_test_2/src/com/bmone/World.java
+++ git_test_2/src/com/bmone/World.java
@@ -8,7 +8,7 @@ public class World {
 		System.out.println("Hi World 2!!!");
 		System.out.println("Hi World 3!!!");
 		System.out.println("Hi World 4!!!");
-	//server change	026fbf99e7c9776729c56207f393a8b87bfd4f3d
+	
 		
 		
 	}
