22e3c2162fb42857d8c65627d878a33d822a7c02
bruce <brucebyu@gmail.com>
2015-02-26 12:25:00 -0800

    merged

--- git_test_3/src/com/bmone/Test.java
+++ git_test_3/src/com/bmone/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test for test3");
 		System.out.println("Edit from TestBranch Again");
+		System.out.println("2nd Developer Edit from TestBranch");
 		
 	}
 
